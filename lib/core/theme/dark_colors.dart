23d24638f0d5eb11fde41a42fd915e7690e39228
Du <119209610+PhanBaDu@users.noreply.github.com>
2026-02-18 13:56:00 +0700

    x

---
+++ lib/core/theme/dark_colors.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+
+class DarkColors {
+  DarkColors._();
+
+  static const Color backgroundDark = Color(0xFF020618);
+
+  static const Color foregroundDark = Color(0xFFf8fafc);
+
+  static const Color cardDark = Color(0xFF0f172b);
+
+  static const Color cardForegroundDark = Color(0xFFf8fafc);
+
+  static const Color primaryDark = Color(0xFFe2e8f0);
+
+  static const Color primaryForegroundDark = Color(0xFF0f172b);
+
+  static const Color secondaryDark = Color(0xFF1d293d);
+
+  static const Color mutedDark = Color(0xFF1d293d);
+
+  static const Color mutedForegroundDark = Color(0xFF90a1b9);
+
+  static const Color destructiveDark = Color(0xFFff6467);
+
+  static const Color borderDark = Color(0x1AFFFFFF);
+
+  static const Color ringDark = Color(0xFF6a7282);
+}
